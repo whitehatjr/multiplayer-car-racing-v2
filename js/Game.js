59ce87c812fb32e456ef607a9ca320f4fa985cc2
vishalgaddam873 <vishal18@navgurukul.org>
2021-04-20 14:13:08 +0530

    Cars images changed

--- js/Game.js
+++ js/Game.js
@@ -15,9 +15,10 @@ class Game {
 
     car1 = createSprite(width / 2 - 50, height - 100);
     car1.addImage("car1", car1_img);
-
+    car1.scale = 0.07;
     car2 = createSprite(width / 2 + 100, height - 100);
     car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
 
     cars = [car1, car2];
   }
