9fb3ef967601445f501fe6deb749fc130f79cca9
vishalgaddam873 <vishal18@navgurukul.org>
2021-04-20 11:01:39 +0530

    Player registration is done

--- js/Form.js
+++ js/Form.js
@@ -2,18 +2,21 @@ class Form {
   constructor() {
     this.input = createInput("").attribute("placeholder", "Enter your name");
     this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
     this.greeting = createElement("h2");
 
     // this.reset = createButton("Reset");
   }
 
   setElementsPosition() {
-    this.input.position(width / 2 - 100, height / 2 - 80);
-    this.playButton.position(width / 2 - 90, height / 2 - 30);
-    this.greeting.position(width / 2 - 300, height / 2 - 200);
+    this.titleImg.position(120, 160);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
 
   setElementsStyle() {
+    this.titleImg.class("gameTitle");
     this.input.class("customInput");
     this.playButton.class("customButton");
     this.greeting.class("greeting");
@@ -31,7 +34,7 @@ class Form {
       this.playButton.hide();
       var message = `
       Hello ${this.input.value()}
-      </br></br>wait for another player to join...`;
+      </br>wait for another player to join...`;
       this.greeting.html(message);
       playerCount += 1;
       player.name = this.input.value();
